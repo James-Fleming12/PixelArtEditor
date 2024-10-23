6383be4895b00782b14fbf9a2d15373b475f3d8a
James <jamesfleming.sd@gmail.com>
2024-10-22 23:18:40 -0700

    got centering working

--- components/panels.cpp
+++ components/panels.cpp
@@ -1,10 +1,10 @@
 #include "../raylib/src/raylib.h"
 #include "panels.h"
-#include <iostream>
 
 void Button::Render() {
     DrawRectangle(x, y, w, h, GRAY);
-    Vector2 mid; mid.x = (x+w)/2.0; mid.y = (y+h)/2.0; // center text in middle of button
-    std::cout << "MidX: " << mid.x << " MidY: " << mid.y << std::endl;
-    DrawText(txt.c_str(), mid.x, mid.y, fontSize, BLACK);
+    int midX = x+w/2;
+    int midY = y+h/2;
+    int wordLen = MeasureText(txt.c_str(), fontSize);
+    DrawText(txt.c_str(), midX-wordLen/2, midY-fontSize/2, fontSize, BLACK);
 }
